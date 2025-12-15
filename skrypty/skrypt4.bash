9227dbd9e3ce11d276d96e7345f3e2e291005ee5
mkrystec <01206347@pw.edu.pl>
2025-12-15 12:27:26 +0100

    poprawka 4

--- skrypty/skrypt4.bash
+++ skrypty/skrypt4.bash
@@ -10,17 +10,17 @@ then
     arg1=$(expr $1 + 1)
     arg2=$(expr $2 + 1)
     arg3=$(expr $3 + 1)
-    echo "Wywolanie 1 (argumenty + 1):"
+    echo "Wywolanie 1 (argumenty + 1; $arg1 $arg2 $arg3):"
     python3 skrypt3.py $arg1 $arg2 $arg3
     arg1=$(expr $1 + 2)
     arg2=$(expr $2 + 2)
     arg3=$(expr $3 + 2)
-    echo "Wywolanie 2 (argumenty + 2):"
+    echo "Wywolanie 2 (argumenty + 2); $arg1 $arg2 $arg3:"
     python3 skrypt3.py $arg1 $arg2 $arg3
     arg1=$(expr $1 + 3)
     arg2=$(expr $2 + 3)
     arg3=$(expr $3 + 3)
-    echo "Wywolanie 3 (argumenty + 3):"
+    echo "Wywolanie 3 (argumenty + 3; $arg1 $arg2 $arg3):"
     python3 skrypt3.py $arg1 $arg2 $arg3
 else
     echo "Blad: Wszystkie argumenty musza byc liczbami dodatnimi"
