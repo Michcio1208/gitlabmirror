7ec9d098226a1ef124ee04f527d05cf2add6ed63
mkrystec <01206347@pw.edu.pl>
2025-12-15 12:06:43 +0100

    Kopie 3 i 4

---
+++ skrypty/skrypt4_kopia.bash
@@ -0,0 +1,27 @@
+#!/bin/bash
+if [ $# -ne 3 ]
+then
+    echo "Podaj dokladnie 3 argumenty"
+    exit 1
+fi
+if [ $1 -gt 0 ] && [ $2 -gt 0 ] && [ $3 -gt 0 ]
+then
+    echo "Argumenty poprawne. Uruchamiam skrypt Python..."
+    arg1=$(expr $1 + 1)
+    arg2=$(expr $2 + 1)
+    arg3=$(expr $3 + 1)
+    echo "Wywolanie 1 (argumenty + 1):"
+    python3 zadanie3.py $arg1 $arg2 $arg3
+    arg1=$(expr $1 + 2)
+    arg2=$(expr $2 + 2)
+    arg3=$(expr $3 + 2)
+    echo "Wywolanie 2 (argumenty + 2):"
+    python3 zadanie3.py $arg1 $arg2 $arg3
+    arg1=$(expr $1 + 3)
+    arg2=$(expr $2 + 3)
+    arg3=$(expr $3 + 3)
+    echo "Wywolanie 3 (argumenty + 3):"
+    python3 zadanie3.py $arg1 $arg2 $arg3
+else
+    echo "Blad: Wszystkie argumenty musza byc liczbami dodatnimi"
+fi
