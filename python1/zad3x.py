aba22c7a783dfd0c6c3100de93b766b48f405657
mkrystec <01206347@pw.edu.pl>
2026-01-05 23:13:49 +0100

    1

--- python1/zad3x.py
+++ python1/zad3x.py
@@ -7,12 +7,12 @@ plik = pd.read_csv('inflacja.csv', sep=';', encoding='cp1250')
 
 
 plik['Wartość_liczba'] = plik['Wartość'].astype(str).str.replace(',', '.').astype(float)
-kolumna_do_analizy = 'Wartość'
+
 
 
 plik = plik.sort_values('Rok')
 
-srednie_wartosci = plik['Wartość'].to_numpy(dtype=float)
+srednie_wartosci = plik['Wartość_liczba'].to_numpy(dtype=float)
 srednia = np.mean(srednie_wartosci)
 odchylenie = np.std(srednie_wartosci, ddof=1)
 
@@ -21,7 +21,7 @@ print(f"Średnia inflacja: {srednia}")
 print(f"Odchylenie standardowe: {odchylenie}")
 
 plt.figure(figsize=(10, 6))
-plt.plot(plik['Rok'], plik[kolumna_do_analizy], color='red', linestyle='-', label='Inflacja')
+plt.plot(plik['Rok'], plik['Wartość_liczba'], color='red', linestyle='-', label='Inflacja')
 plt.title('Wskaźnik inflacji (GUS)')
 plt.xlabel('Rok')
 plt.ylabel('Wartość')
