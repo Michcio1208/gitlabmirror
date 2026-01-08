6bb3e735ba2207ff3825ff78cd26271636119472
mkrystec <01206347@pw.edu.pl>
2026-01-08 18:58:38 +0100

    zrobione

--- python2/zrobione/zad2.py
+++ python2/zrobione/zad2.py
@@ -13,8 +13,8 @@ y_env_lower = -A * np.exp(-lambda_coeff * x)
 plt.figure(figsize=(10, 6))
 
 plt.plot(x, y_sin, label='Tłumiona sinusoida', linestyle='-')
-plt.plot(x, y_env_upper, label=r'Gorna obwiednia $A e^{-\lambda x}$', linestyle='--', color='red')
-plt.plot(x, y_env_lower, label=r'Dolna obwiednia $-A e^{-\lambda x}$', linestyle='--', color='red')
+plt.plot(x, y_env_upper, label=r'Gorna obwiednia $A e^{-\lambda x}$', linestyle='-.', color='red')
+plt.plot(x, y_env_lower, label=r'Dolna obwiednia $-A e^{-\lambda x}$', linestyle='--', color='orange')
 
 plt.title("Wykres tłumionej sinusoidy")
 plt.xlabel("Czas (t)")
